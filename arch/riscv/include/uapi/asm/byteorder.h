72476a128366e78fade6bbd1060556fc7e9c1296
Djordje Todorovic <djordje.todorovic@htecgroup.com>
2024-08-14 15:56:39 +0200

    arch/riscv: Ensure proper UAPI endianess

--- arch/riscv/include/uapi/asm/byteorder.h
+++ arch/riscv/include/uapi/asm/byteorder.h
@@ -7,6 +7,10 @@
 #ifndef _UAPI_ASM_RISCV_BYTEORDER_H
 #define _UAPI_ASM_RISCV_BYTEORDER_H
 
+#ifdef CONFIG_CPU_BIG_ENDIAN
+#include <linux/byteorder/big_endian.h>
+#else
 #include <linux/byteorder/little_endian.h>
+#endif
 
 #endif /* _UAPI_ASM_RISCV_BYTEORDER_H */
